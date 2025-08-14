c5085c2640dcce9d642178f17e6c92e690292009
Jose Gabriel <josegabrielg2014@gmail.com>
2025-08-14 20:44:10 -0300

    agora ta funcionando tudo com ponteiro

--- main.cpp
+++ main.cpp
@@ -1,10 +1,60 @@
 #include <iostream>
 #include <algorithm>
-#include <cmath>
 
 using namespace std;
 
-void receberValores()
+void rgbParaHSV(double red, double green, double blue, double &h, double &s, double &v){
+        double r = red / 255.0;
+        double g = green / 255.0;
+        double b = blue / 255.0;
+    
+        double valorMAX = max({r, g, b});
+        double valorMIN = min({r, g, b});
+        double diferenca = valorMAX - valorMIN;
+    
+        
+        // V
+        v = valorMAX;
+    
+        // S
+        if (valorMAX == 0)
+        {
+            s = 0;
+        }
+        else
+        {
+            s = diferenca / valorMAX;
+        }
+    
+        // Matiz
+        if (diferenca == 0)
+        {
+            h = 0;
+        }
+        else if (valorMAX == r)
+        {
+            h = 60 * ((g - b) / diferenca);
+        }
+        else if (valorMAX == g)
+        {
+            h = 60 * ((b - r) / diferenca + 2);
+        }
+        else
+        {
+            h = 60 * ((r - g) / diferenca + 4);
+        }
+    
+
+        if (h < 0)
+        {
+            h += 360;
+        }
+    
+        cout << "HSV GERADO: H = " << h << ", S = " << s * 100 << "%, V = " << v * 100 << "%. \n\n";
+    
+}
+
+void receberValores(double &h, double &s, double &v)
 {
     double red, green, blue;
     cout << "\nInsira o valor de red: ";
@@ -16,57 +66,13 @@ void receberValores()
 
     cout << "RED: " << red << ", GREEN: " << green << ", BLUE: " << blue << endl;
 
-    double r = red / 255.0;
-    double g = green / 255.0;
-    double b = blue / 255.0;
-
-    double valorMAX = max({r, g, b});
-    double valorMIN = min({r, g, b});
-    double diferenca = valorMAX - valorMIN;
-
-    double h, s, v;
-
-    // V
-    v = valorMAX;
-
-    // S
-    if (valorMAX == 0)
-    {
-        s = 0;
-    }
-    else
-    {
-        s = diferenca / valorMAX;
-    }
-
-    // Matiz
-    if (diferenca == 0)
-    {
-        h = 0;
-    }
-    else if (valorMAX == r)
-    {
-        h = 60 * ((g - b) / diferenca);
-    }
-    else if (valorMAX == g)
-    {
-        h = 60 * ((b - r) / diferenca + 2);
-    }
-    else
-    {
-        h = 60 * ((r - g) / diferenca + 4);
-    }
-
-    if (h < 0)
-    {
-        h += 360;
-    }
-
-    cout << "HSV GERADO: H = " << h << ", S = " << s * 100 << "%, V = " << v * 100 << "%. \n\n";
+    rgbParaHSV(red, green, blue, h, s, v);
 }
+
 
 int main()
 {
+    double h, s, v;
     cout << "Inicio do Conversor de RGB para HSV. \n\n";
     cout << "1 - Iniciar conversao\n";
     cout << "0 - Sair do programa\n";
@@ -74,7 +80,7 @@ int main()
     cin >> opcao;
     if (opcao)
     {
-        receberValores();
+        receberValores(h, s,v );
     }
     else
     {
